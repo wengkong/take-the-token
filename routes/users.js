ae85f05bafadcba26736a55613fb8f03e0a1b444
Kam Weng Kong <wengkong.kam@gmail.com>
2019-09-18 16:27:50 +0800

    Update register user

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,32 @@ const router = require('express').Router()
 const db = require('../db/index')
 
 router.post('/register', (req, res) => {
-    db('users').insert({
-        email: req.body.email,
-        password: req.body.password,
-        name: req.body.name
+    // Check if user exists
+    db('users')
+    .where('email', req.body.email)
+    .first('email')
+    .then(rows => {
+        // User not exists
+        if (!rows) {
+            // Create user
+            db('users').insert({
+                email: req.body.email,
+                password: req.body.password,
+                name: req.body.name
+            })
+            .then((id) => {
+                res.send(`Register user with ${id}`)
+            })
+            .catch((err) => {
+                res.send(err)
+            })
+        }
+        // User already exists
+        else {
+            res.send(`User with email ${rows.email} already exists`)    
+        }
     })
-    .then((id) => {
-        res.send(`Register user with ${id}`)
-    })
-    .catch((err) => {
+    .catch(err => {
         console.log(err)
         res.send(err)
     })
